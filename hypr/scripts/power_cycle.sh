e9afcbd7e55c5f2603bef0a35e4d3ce9cfcffca0
Ghifaryh <mhghifaryy@gmail.com>
2026-03-20 22:59:32 +0700

    removing the monitor refresh rate change on powerscycle

--- hypr/scripts/power_cycle.sh
+++ hypr/scripts/power_cycle.sh
@@ -2,15 +2,15 @@
 CURRENT=$(powerprofilesctl get)
 
 if [ "$CURRENT" = "power-saver" ]; then
-    powerprofilesctl set balanced
-    hyprctl keyword monitor eDP-1,2880x1800@60,0x0,1.5
-    notify-send "Mode: Balanced" "60Hz | CPU: Balanced" -i balanced-profile
+  powerprofilesctl set balanced
+  # hyprctl keyword monitor eDP-1,2880x1800@60,0x0,1.5
+  notify-send "Mode: Balanced" "CPU: Balanced" -i balanced-profile
 elif [ "$CURRENT" = "balanced" ]; then
-    powerprofilesctl set performance
-    hyprctl keyword monitor eDP-1,2880x1800@90,0x0,1.5
-    notify-send "Mode: Performance" "90Hz | CPU: High" -i performance-profile
+  powerprofilesctl set performance
+  # hyprctl keyword monitor eDP-1,2880x1800@90,0x0,1.5
+  notify-send "Mode: Performance" "CPU: High" -i performance-profile
 else
-    powerprofilesctl set power-saver
-    hyprctl keyword monitor eDP-1,2880x1800@60,0x0,1.5
-    notify-send "Mode: Power Save" "60Hz | CPU: Low" -i battery-low
+  powerprofilesctl set power-saver
+  # hyprctl keyword monitor eDP-1,2880x1800@60,0x0,1.5
+  notify-send "Mode: Power Save" "CPU: Low" -i battery-low
 fi
