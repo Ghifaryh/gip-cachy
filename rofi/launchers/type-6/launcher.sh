1013d85fcb4e2f27ca30035461d314b3b871cca2
Ghifaryh <mhghifaryy@gmail.com>
2026-08-25 17:24:07 +0700

    feat(rofi): add toggle logic to type-6 launcher

--- rofi/launchers/type-6/launcher.sh
+++ rofi/launchers/type-6/launcher.sh
@@ -2,18 +2,17 @@
 
 ## Author : Aditya Shakya (adi1090x)
 ## Github : @adi1090x
-#
-## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
-#
-## Available Styles
-#
-## style-1     style-2     style-3     style-4     style-5
-## style-6     style-7     style-8     style-9     style-10
+
+# Toggle: If rofi is already open, kill it and exit
+if pgrep -x "rofi" >/dev/null; then
+  pkill -x "rofi"
+  exit 0
+fi
 
 dir="$HOME/.config/rofi/launchers/type-6"
 theme='style-1'
 
 ## Run
 rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
+  -show drun \
+  -theme ${dir}/${theme}.rasi
